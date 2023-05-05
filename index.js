2db775d7254bd4630498a6ce12c6061df577b6c6
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-05-05 01:46:35 -0500

    installs and more bug fixes

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const inquirer = require('inquirer');
-const db = require('./db/connect');
 require('console.table');
+const mysql = require('mysql');
+const logo = require('asciiart-logo');
 
 // connect to sql database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: process.env.PASSWORD,
-        database: 'company_db'
-    },
-    console.log(`Connected to the company_db database.`)
-);
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.PASSWORD,
+    database: 'company_db'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected to the company_db database.');
+});
 
 startApp()
 
